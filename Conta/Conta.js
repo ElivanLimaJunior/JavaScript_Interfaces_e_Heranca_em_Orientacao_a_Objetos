514863578ba6a8d305440ac60b13e996b20187cd
Elivan-Jr <elivanlimajunior@gmail.com>
2022-02-26 17:33:15 -0300

    O que aprendi.
    Especializando código
    
    Herdando propriedades e atributos
    
    Sobrecarga de métodos
    
    Compartilhando código de forma segura
    
    Mantendo nosso código na linguagem do cliente
    
    Evitando código muito complicado através da herança
    
    Palavra-chave Super
    
    Método e propriedades privadas e protegidas

--- Conta/Conta.js
+++ Conta/Conta.js
@@ -1,44 +1,46 @@
-export class Conta{
-    constructor(tipo, saldoInicial, cliente, agencia){
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
         this._saldo = saldoInicial;
         this._cliente = cliente;
-        this.agencia = agencia;
+        this._agencia = agencia;
+       
     }
 
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
             this._cliente = novoValor;
         }
     }
 
-    get cliente(){
+    get cliente() {
         return this._cliente;
     }
 
-    get saldo(){
+    get saldo() {
         return this._saldo;
     }
 
-    sacar(valor){
-        let taxa = 1;
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+    sacar(valor) {
+        let taxa = 1
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
         }
+
+        return 0;
     }
 
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
-        this._saldo += valor;           
+    depositar(valor) {
+        this._saldo += valor;
     }
 
-    tranferir(valor, conta){
-        
+    tranferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        
     }
 }
