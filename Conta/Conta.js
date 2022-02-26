8686dd833b7388994fbffdfa273860ca394b2512
Elivan-Jr <elivanlimajunior@gmail.com>
2022-02-26 17:07:44 -0300

    Por que não usar tantos if's ?
    o meu código ainda não fica maravilhoso, porque eu tenho muitas condicionais dentro do mesmo código, muitos caminhos possíveis dentro desse método sacar(), muitos caminhos possíveis dento do depositar(), e eu vou ter esse if todas as vezes que eu tiver alguma regra especial para algum comportamento especial de uma conta que o ByteBank quiser. Então não é a melhor maneira de fazermos isso.

--- Conta/Conta.js
+++ Conta/Conta.js
@@ -1,12 +1,36 @@
 export class Conta{
-    constructor(saldoInicial, cliente, agencia){
+    constructor(tipo, saldoInicial, cliente, agencia){
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this.agencia = agencia;
+        this._tipo = tipo;
+    }
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
     }
 
     sacar(valor){
-        taxa = 1.1* valor;
+        let taxa = 1;
+        if(this._tipo == "corrrente"){
+            taxa = 1.1;
+        }
+        if(this._tipo == "salario"){
+            taxa = 1;
+        }
+        if(this._tipo == "empresarial"){
+            taxa = 1.15;
+        }
         if(this._saldo >= valor){
             this._saldo -= valor;
             return valor;
--- Conta/ContaCorrente.js
+++ Conta/ContaCorrente.js
@@ -6,19 +6,7 @@ export class ContaCorrente{
     
 
 
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
-            this._cliente = novoValor;
-        }
-    }
 
-    get cliente(){
-        return this._cliente;
-    }
-
-    get saldo(){
-        return this._saldo;
-    }
 
     constructor(agencia, cliente){
         this.agencia = agencia;
