91f6dc2987715b73f1eab34ff82ec98fc79ecc6d
Elivan-Jr <elivanlimajunior@gmail.com>
2022-03-01 13:43:45 -0300

    Sistema de Autenticação de Senha
    Criando um Sistema Simples para autenticação de senhas.

---
+++ index.js
@@ -0,0 +1,16 @@
+import {Cliente} from "./Conta/Cliente.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456") //Cadastrar Senha
+
+const gerente = new Gerente("Ricardo", 5000, 12345678901);
+gerente.cadastrarSenha("123");
+
+const estaLogado = SistemaAutenticacao.login(diretor, "123456"); //Confirmar Senha
+const estaLogadogerente = SistemaAutenticacao.login(gerente, "123");
+
+console.log(estaLogado);
+console.log(estaLogadogerente);
